46deb1d1f4f13b754df948dcd6fd375d6846faa5
20MH1A0438 <81519524+20MH1A0438@users.noreply.github.com>
2022-08-25 18:53:43 +0530

    t=int(input()) l=list(map(int,input().split())) l.sort() a=int(input()) mid=t//2 l1=0 r=t while t>1:     if a==l[mid]:         print(True)         break     elif a>l[mid]:         l1=mid         mid=(l1+r)//2     else:         r=mid         mid=(l1+r)//2     if mid==l1 or mid==r:         print(False)         break

---
+++ element_search_in_an_array.py
@@ -0,0 +1,21 @@
+t=int(input())
+l=list(map(int,input().split()))
+l.sort()
+a=int(input())
+mid=t//2
+l1=0
+r=t
+while t>1:
+    if a==l[mid]:
+        print(True)
+        break
+    elif a>l[mid]:
+        l1=mid
+        mid=(l1+r)//2
+    else:
+        r=mid
+        mid=(l1+r)//2
+    if mid==l1 or mid==r:
+        print(False)
+        break
+        
