0581de3e7ba177c40290d5b6c0f344c83b82c972
20MH1A0438 <81519524+20MH1A0438@users.noreply.github.com>
2022-10-13 05:53:47 +0530

    n=input() n=n.lower() d=n.split(' ') m='aeiou' dic={} for i in d:     c=0     for j in i:         if j in m:             c+=1     dic[i]=c co=min(list(dic.values())) print(co)

---
+++ minimum_vowel_count_in_a_word.py
@@ -0,0 +1,14 @@
+n=input()
+n=n.lower()
+d=n.split(' ')
+m='aeiou'
+dic={}
+for i in d:
+    c=0
+    for j in i:
+        if j in m:
+            c+=1
+    dic[i]=c
+co=min(list(dic.values()))
+print(co)
+        
