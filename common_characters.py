61607aff24a3900e527a70818a76ba8d8773180a
20MH1A0438 <81519524+20MH1A0438@users.noreply.github.com>
2022-10-15 14:12:03 +0530

    s1=input() s2=input() s1=s1.lower() s1=s1.replace(" ","") s2=s2.lower() s2=s2.replace(" ","") dic={} s='' for i in s1:     if i not in dic:         dic[i]=1 for j in s2:     if j not in dic:         pass     else:         dic[j]+=1 for k,v in dic.items():     if v>=2:         s+=k if len(s)==0:     print('-1') else:     ans=sorted(s)     print("".join(ans))

---
+++ common_characters.py
@@ -0,0 +1,24 @@
+s1=input()
+s2=input()
+s1=s1.lower()
+s1=s1.replace(" ","")
+s2=s2.lower()
+s2=s2.replace(" ","")
+dic={}
+s=''
+for i in s1:
+    if i not in dic:
+        dic[i]=1
+for j in s2:
+    if j not in dic:
+        pass
+    else:
+        dic[j]+=1
+for k,v in dic.items():
+    if v>=2:
+        s+=k
+if len(s)==0:
+    print('-1')
+else:
+    ans=sorted(s)
+    print("".join(ans))
