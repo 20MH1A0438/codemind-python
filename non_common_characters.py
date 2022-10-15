bede4a2fccc1c18a8385ac642cb9f227bb86c5a1
20MH1A0438 <81519524+20MH1A0438@users.noreply.github.com>
2022-10-15 06:17:53 +0530

    s1=input() s2=input() s1=s1.lower() s1=s1.replace(" ","") s2=s2.lower() s2=s2.replace(" ","") dic={} s='' for i in s1:     if i not in dic:         dic[i]=1 for j in s2:     if j not in dic and j not in s:         s+=j     elif j not in dic and j in s:         pass     else:         dic[j]+=1 for k,v in dic.items():     if v==1:         s+=k ans=sorted(s) print("".join(ans))

---
+++ non_common_characters.py
@@ -0,0 +1,23 @@
+s1=input()
+s2=input()
+s1=s1.lower()
+s1=s1.replace(" ","")
+s2=s2.lower()
+s2=s2.replace(" ","")
+dic={}
+s=''
+for i in s1:
+    if i not in dic:
+        dic[i]=1
+for j in s2:
+    if j not in dic and j not in s:
+        s+=j
+    elif j not in dic and j in s:
+        pass
+    else:
+        dic[j]+=1
+for k,v in dic.items():
+    if v==1:
+        s+=k
+ans=sorted(s)
+print("".join(ans))
